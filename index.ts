bf0a2c3d192daff0bfb3b32d2a868e96b67ad837
ZayedShahcode <reachzayed@proton.me>
2026-01-29 22:35:18 +0530

    Setup server

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { connectDB } from "./src/config/database";
+import app from "./src/app";
+
+
+const PORT = process.env.PORT || 3000;
+
+connectDB()
+.then(()=>{
+    app.listen(PORT,()=>{
+        console.log("SERVER is running on Port",PORT);
+    })
+})
